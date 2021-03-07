041e8222efaf6130664200cb8c9842b048590373
tilashini28 <33094831+tilashini28@users.noreply.github.com>
2021-03-08 03:39:32 +0800

    Add files via upload

---
+++ managerLogin1.php
@@ -0,0 +1,30 @@
+<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 login-left">
+				<form class="login100-form validate-form p-l-55 p-r-55 p-t-178"  method = "post" action="managerValidation1.php">
+					<span class="login100-form-title">
+						Login As Manager
+					</span>
+
+					<div class="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
+						<input class="input100" type="text" name="managerUsername" placeholder="managerUsername">
+						<span class="focus-input100"></span>
+					</div>
+					
+					<div class="wrap-input100 validate-input" data-validate = "Please enter password">
+						<input class="input100" type="password" name="password" placeholder="Password">
+						<span class="focus-input100"></span>
+					</div>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn" name="loginManager" type="submit" value="loginManager">
+						
+							Login As Manager
+						</button>
+					</div>
+					</div>
+				</form>
+			</div>
+		</div>
+
+		
+	</div>
